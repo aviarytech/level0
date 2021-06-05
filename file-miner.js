917cd695172db28e9494ad9414ab9a55d2e6390f
Brian Richter <brianorwhatever@gmail.com>
2021-06-05 16:57:28 -0700

    windows mining

--- file-miner.js
+++ file-miner.js
@@ -1,33 +1,34 @@
 const chokidar = require("chokidar");
 const axios = require("axios");
 const fs = require("fs");
+const path = require('path');
 
 // Initialize watcher.
-const watcher = chokidar.watch("/Users/brian/Downloads/[a-z0-9]**.json", {
+const watcher = chokidar.watch("C:/Users/brian/Downloads/[a-z0-9]**.json", {
   ignored: /(^|[\/\\])\../, // ignore dotfiles
   persistent: true,
 });
 // Something to use when events are received.
 const log = console.log.bind(console);
 // Add event listeners.
-watcher.on("add", async (path) => {
-  log(`File ${path} has been added`);
+watcher.on("add", async (filepath) => {
+  log(`File ${filepath} has been added`);
   // Use fs.readFile() method to read the file
-  fs.readFile(path, "utf8", async (err, data) => {
-    const id = path.split("/").pop().split(".")[0];
+  fs.readFile(filepath, "utf8", async (err, data) => {
+    const id = path.parse(filepath).name;
     try {
       JSON.parse(data).forEach(async (obj) => {
         console.log("sending!");
         try {
-          await axios.post(`http://localhost:3000/${id}.json`, { ...obj });
+          await axios.post(`https://0.a51.io/${id}.json`, { ...obj });
         } catch (e) {
           console.log(e);
         }
       });
-      fs.unlinkSync(path);
+      fs.unlinkSync(filepath);
     } catch (e) {
       console.log(JSON.stringify(e));
-      fs.unlinkSync(path);
+      fs.unlinkSync(filepath);
     }
   });
 });
